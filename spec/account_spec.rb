231c0063e405b32e819b7d11d1569126ec4ad562
benauld <ben@portobello-consultancy.co.uk>
2020-02-24 13:22:55 +0000

    can do multiple withdrawals and deposits

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -21,5 +21,22 @@ describe Account do
       subject = Account.new
       expect(subject.withdraw(14-01-2012, 500)) .to eq(-500)
     end
+
+    it "can do multiple withdrawals" do
+      subject = Account.new
+      subject.withdraw(13-01-2012, 500)
+      expect(subject.withdraw(14-01-2012, 500)) .to eq(-1000)
+    end  
   end
+
+  describe "deposit and withdrawal" do
+    it "can keep track of multiple deposits and withdrawals" do
+      subject = Account.new
+      subject.deposit(10-01-2012, 1000)
+      subject.withdraw(13-01-2012, 500)
+      subject.deposit(10-01-2012, 1000)
+      expect(subject.withdraw(14-01-2012, 500)) .to eq(1000) 
+    end
+  end
+  
 end
