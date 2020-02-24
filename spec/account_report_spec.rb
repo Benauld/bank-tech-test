cff214d3f91fd59e7e7b4a25e3ed330f8df6ae2d
benauld <ben@portobello-consultancy.co.uk>
2020-02-24 14:22:14 +0000

    added a stub for account in rspec for account_report

--- spec/account_report_spec.rb
+++ spec/account_report_spec.rb
@@ -3,12 +3,23 @@
 require 'account_report'
 require 'account'
 
+
+
 describe AccountReport do
+  let(:account) {double( :account => [0,0,0,0])}
   describe 'print' do
     it 'prints a basic header when no account exists' do
       subject = AccountReport.new
-      account = Account.new
       expect{ subject.print(account).to output("date || credit || debit || balance").to_stdout }
     end
+
+    # it 'prints a single line account exists' do
+    #   subject = AccountReport.new
+    #   account = Account.new
+      
+    #   expect{ subject.print(account).to output("date || credit || debit || balance").to_stdout }
+    # end
   end
+
+  
 end
