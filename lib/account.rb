345dd638e58a6bed5a48f5d1a986bc55f81a54cc
benauld <ben@portobello-consultancy.co.uk>
2020-02-24 12:18:36 +0000

    can deposit multiple times

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,15 @@
 class Account
 
   def initialize()
+    @transation = []
+    @transactions = [@transaction]
     @current_balance = 0
   end
 
   def deposit(date, amount)
     @current_balance += amount
+    @transaction = [date, amount, 0, @current_balance]
+    @transactions.insert(0, @transaction)
+    return @current_balance
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,9 +4,16 @@ describe Account do
 
   describe "deposit" do
     
-    it "has a balance of 1000 when 100 deposited" do
+    it "has a balance of 1000 when 1000 deposited" do
       subject = Account.new
       expect(subject.deposit(10-01-2012, 1000)) .to eq(1000)
     end
+
+    it "can store multiple deposits and updates balance" do
+    subject = Account.new
+    subject.deposit(10-01-2012, 1000)
+    expect(subject.deposit(11-01-2012, 1000)) .to eq(2000)
+    end
+
   end
 end
